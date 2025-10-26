d7808166141aec731b1c2bcbed869ca5d227eff8
YukiCS <alejomorell@gmail.com>
2025-10-26 21:47:43 +0100

    feat: improved pricing legibility

--- src/main.sh
+++ src/main.sh
@@ -20,7 +20,9 @@ cat response.json | jq -e '.deck.authorTags | to_entries[] ' | \
 	jq 'walk(if type == "object" then with_entries( if .key == "key" then .key = "name" else . end ) else . end)' | \
 	jq 'walk(if type == "object" then with_entries( if .key == "value" then .key = "card.tags" else . end ) else . end)' | jq -s > intermediate/tags.json
 ## merges tags and cards
-jq --slurpfile uid intermediate/cards.json '($uid[0] | INDEX(."card.name")) as $dict  | map( $dict[.name] + del(.name) )' intermediate/tags.json > intermediate/merge.json
+## and removes extra decimals from prices
+jq --slurpfile uid intermediate/cards.json '($uid[0] | INDEX(."card.name")) as $dict  | map( $dict[.name] + del(.name) )' intermediate/tags.json \
+	| sed -E 's/([-+]?[0-9]+\.[0-9]{2})[0-9]{2}\b/\1/g' > intermediate/merge.json
 
 echo "ALL PROXYS $(cat intermediate/merge.json | jq '.[] | select( [ ."card.tags"[] | contains("proxy") ] | any) | select(has("card.name"))'| jq -s length)"
 echo
@@ -48,21 +50,13 @@ echo
 mkdir -p tagged
 for TAG in $(cat intermediate/tags.json | jq '.[]."card.tags"[]' | sort -u | tr -d '"' | grep -vE $BASIC_FILTER_REGEX); do
 
-	# if [[ "$1" = "table" ]]; then
 	cat intermediate/merge.json | jq -r --arg CURRENT_TAG "$TAG" '.[]
 		| select((.["card.tags"] | index($CURRENT_TAG))
 		and (.["card.tags"] | index("proxy") | not)
 		and (.["card.tags"] | index("bought") | not))
 		| select(has("card.name"))
 		| "\(."quantity");\(."card.name");(\(."card.set"));\(."card.cn");#;\(."card.prices.ct")€;\(."card.prices.ck")€"' | tr -d '"' > tagged/$TAG.json
-	# else
-	# cat intermediate/merge.json | jq -r --arg CURRENT_TAG "$TAG" '.[]
-	# 	| select((.["card.tags"] | index($CURRENT_TAG))
-	# 	and (.["card.tags"] | index("proxy") | not)
-	# 	and (.["card.tags"] | index("bought") | not))
-	# 	| select(has("card.name"))
-	# 	| "\(."quantity") \(."card.name") (\(."card.set")) \(."card.cn") \t\t\t # CardTrader: \(."card.prices.ct")€ CardMarket: \(."card.prices.ck")€"' | tr -d '"' > tagged/$TAG.json
-	# fi
+
 	echo "$(echo $TAG | sed 's/.*/\U&/g') $(cat tagged/$TAG.json | wc -l)"
 	echo
 	if [[ "$1" == "table" ]]; then
